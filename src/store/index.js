ec86ee649b2de87c99e43c08bc3dffac796fe9b9
AlenaNik <alena.nik.digital@gmail.com>
2020-06-01 18:02:57 +0200

    post event

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from "../services/EventService";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    emoji: []
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: []
   },
   mutations: {
-    INCREMENT_EMOJI(state, value) {
-      state.emoji.push(value)
+    ADD_EVENT(state, event) {
+      state.events.push(event)
     }
   },
   actions: {
-    updateEmoji({state, commit}, value) {
-      if(state.emoji) {
-        commit('INCREMENT_EMOJI', value)
-      }
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
     }
   },
-  modules: {}
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
